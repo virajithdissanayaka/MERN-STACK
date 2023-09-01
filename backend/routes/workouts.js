713f2c672b5543e0af0d892a5733f094011ba4ed
virajithdissanayaka <dissnayakavirajith11@gmail.com>
2023-09-01 12:27:02 +0530

    New Backend

---
+++ backend/routes/workouts.js
@@ -0,0 +1,32 @@
+const express = require('express')
+
+
+const router = express.Router()
+
+
+//GET all workouts
+router.get('/', (req,res) => {
+    res.json({mssg: 'Get all workouts'})
+})
+
+//GET a single workout
+router.get('/:id', (req,res) => {
+    res.json({mssg: 'Get a  single workout'})
+}) 
+
+//POST a new workout
+router.post('/', async (req,res) => {
+    
+})
+
+//DELETE a workout
+router.delete('/:id', (req,res) => {
+    res.json({mssg: 'DELETE a workout'})
+})
+
+//UPDATE a workout
+router.patch('/:id', (req,res) => {
+    res.json({mssg: 'UPDATE a workout'})
+})
+
+module.exports = router
